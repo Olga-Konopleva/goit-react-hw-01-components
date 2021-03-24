77c3641c4fe4139e369612b8eb9248fac2137bfe
Olga Konopleva <olenenok.she@gmail.com>
2021-03-24 14:30:12 +0200

    test husky

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import Profile from "./components/Profile/Profile";
-import user from "./user.json";
-
+import Profile from './components/Profile/Profile';
+import Statistics from './components/Statistics/Statistics';
+import user from './user.json';
+import statsData from './statistical-data.json';
 
 const App = () => {
   return (
     <div>
-      <Profile 
-      name={user.name}
-      tag={user.tag}
-      location={user.location}
-      avatar={user.avatar}
-      stats={user.stats} />
+      <Profile
+        name={user.name}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={statsData} />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,16 @@
+const Statistics = ({ title, stats }) => (
+  <section className="statistics">
+    <h2 className="title">{title}</h2>
+
+    <ul className="stat-list">
+      {stats.map(({ id, label, percentage }) => (
+        <li className="item" key={id}>
+          <span className="label">{label}</span>
+          <span className="percentage">{percentage}%</span>
+        </li>
+      ))}
+    </ul>
+  </section>
+);
+
+export default Statistics;
